da099f55f9a102c5535f8642a18ccc661de17f16
Muhammedbadr <111696961+Muhammedbadr@users.noreply.github.com>
2022-11-17 17:57:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+<div class="ph">
+                <div class="card">
+                    <i class="font_phone">
+                        <span class="material-symbols-outlined"  >
+                            mail
+                        </span>
+                    </i>
+                    <div>
+                        <h3>Email</h3>
+                        <p>muhammedbadr@1234gamil.com</p>
+                    </div>
+                </div>
+            </div>
